3cce2bf9b2a8a9379bb62aa927f3e943fe55ad85
David Resnick <dresnick@salesforce.com>
2022-03-19 10:07:33 +0200

    with deck names

--- anki-wiki/model.py
+++ anki-wiki/model.py
@@ -1,31 +1,40 @@
 import genanki
 
+TOP_LEVEL_DECK_NAME = "Thai dishes"
+YAML_FILE_NAME = "List_of_Thai_dishes.yaml"
+WIKIPEDIA_URL = "https://en.wikipedia.org/wiki/List_of_Thai_dishes"
+
 THAI_FOOD_MODEL = genanki.Model(
     2081520238,
     "Thai Food",
     fields=[
-        {"name": "Thai name"},
-        {"name": "Thai script"},
-        {"name": "English name"},
-        {"name": "Image"},
-        {"name": "Recording"},
-        {"name": "Region"},
-        {"name": "Description"},
+        {"name": "Thai name", "font": "Arial"},
+        {"name": "Thai script", "font": "Arial"},
+        {"name": "English name", "font": "Arial"},
+        {"name": "Image", "font": "Arial"},
+        {"name": "Recording", "font": "Arial"},
+        {"name": "Region", "font": "Arial"},
+        {"name": "Description", "font": "Arial"},
     ],
     templates=[
         {
             "name": "Food name",
             "qfmt": "{{Thai script}}",
-            "afmt": "".join[
-                "{{FrontSide}}",
-                "<hr id='answer'>",
-                "{{English name}}",
-                "<br/><img src='{{Image}}'>}",
-                "<br/>{{Thai name}}",
-                "[sound:{{Recording}}]",
-                "<br/>{{Description}}",
-                "<br/>{{Region}}",
-            ],
+            "afmt": "{{FrontSide}}"
+            "<hr id='answer'>"
+            "{{English name}}"
+            "<br/>{{Image}}"
+            "<br/>{{Thai name}} {{Recording}}"
+            "<br/>{{Description}}"
+            "<br/>{{Region}}",
         },
     ],
+    css=".card {\n font-family: arial;\n font-size: 20px;\n text-align: center;\n color: black;\n background-color: white;\n}\n\n"
+    ".cloze {\n font-weight: bold;\n color: blue;\n}\n.nightMode .cloze {\n color: lightblue;\n}",
+)
+
+THAI_FOOD_DECK = genanki.Deck(
+    1955603963,
+    "Thai Dishes",
+    "List from https://en.wikipedia.org/wiki/List_of_Thai_dishes",
 )
--- anki-wiki/scrape.py
+++ anki-wiki/scrape.py
@@ -3,11 +3,13 @@ import genanki
 import os
 import pandas as pd  # library for data analysis
 import requests  # library to handle requests
+import random
 import shutil
 import tempfile
 import yaml
 from bs4 import BeautifulSoup  # library to parse HTML documents
 from urllib.parse import urlparse
+from model import THAI_FOOD_MODEL, TOP_LEVEL_DECK_NAME, YAML_FILE_NAME, WIKIPEDIA_URL
 
 
 def get_image(row, working_dir):
@@ -29,7 +31,7 @@ def get_image(row, working_dir):
     return image_path
 
 
-def convert_to_deck(table):
+def soup_to_panda(table):
     df = pd.DataFrame(pd.read_html(str(table))[0])
     rows = table.find("tbody").findAll("tr")[1:]
     assert len(df) == len(rows)
@@ -37,23 +39,54 @@ def convert_to_deck(table):
     for index, row in enumerate(rows):
         image_path = get_image(row, working_dir)
         df.at[index, "Image"] = image_path
+    return df
+
+
+def panda_to_anki_deck(df, name):
+
+    pass
+
+
+def extract_headline(heading):
+    return heading.find("span", {"class": "mw-headline"}).text
+
+
+def build_deck_name(table, soup):
+    element = table.previous_sibling
+    title = TOP_LEVEL_DECK_NAME
+    subtitle = None
+    while element.name != "h2":
+        if subtitle is None and element.name == "h3":
+            subtitle = extract_headline(element)
+        element = element.previous_sibling
+    title = f"{ TOP_LEVEL_DECK_NAME }::{ extract_headline(element) }"
+    if subtitle:
+        title = f"{ title }::{ subtitle }"
+    return title
+
+
+def get_deck_id(deck_name):
+    with open(YAML_FILE_NAME, "r") as file:
+        config = yaml.safe_load(file)
+    if "decks" not in config:
+        config["decks"] = {}
+    if deck_name not in config["decks"]:
+        config["decks"][deck_name] = random.randrange(1 << 30, 1 << 31)
+        with open(YAML_FILE_NAME, "w") as file:
+            file.write(yaml.dump(config))
+    return config["decks"][deck_name]
 
 
 def main():
-    parser = argparse.ArgumentParser(
-        description="Create Anki decks from wikipedia tables."
-    )
-    parser.add_argument(
-        "yaml_config", type=argparse.FileType("r"), help="yaml configuration file"
-    )
-    args = parser.parse_args()
-    config = yaml.safe_load(args.yaml_config)
-
-    response = requests.get(config["url"])
+    response = requests.get(WIKIPEDIA_URL)
     soup = BeautifulSoup(response.text, "html.parser")
     for table in soup.find_all("table", {"class": "wikitable"}):
-        convert_to_deck(table)
-        break
+        # df = soup_to_panda(table)
+        deck_name = build_deck_name(table, soup)
+        print(deck_name)
+        deck_id = get_deck_id(deck_name)
+        # panda_to_anki_deck(df, deck_name)
+        # break
 
 
 if __name__ == "__main__":
